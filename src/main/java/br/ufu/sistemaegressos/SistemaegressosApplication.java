e5d4f912e21a3b6086685946ac8791f1d176c707
Vitória <vitoriasilvacardoso36@gmail.com>
2024-10-21 14:17:53 -0300

    Iniciando spring

---
+++ src/main/java/br/ufu/sistemaegressos/SistemaegressosApplication.java
@@ -0,0 +1,13 @@
+package br.ufu.sistemaegressos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaegressosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaegressosApplication.class, args);
+	}
+
+}
